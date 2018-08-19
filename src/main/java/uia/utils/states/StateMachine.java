01dd1cfa8422e7d629764a7418842a4045423006
Kyle K. Lin <gazer.kanlin@gmail.com>
2018-08-19 19:47:53 +0800

    0.2.0, Update License

--- src/main/java/uia/utils/states/StateMachine.java
+++ src/main/java/uia/utils/states/StateMachine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2017 UIA
+ * Copyright 2018 UIA
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
